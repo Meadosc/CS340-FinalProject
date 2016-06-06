a0a4a9dc7672780ba51cbe041b0ab248e094cec0
Meadosc <meadosc@onid.orst.edu>
2016-06-05 23:47:45 -0700

    Update addInuryRecovery.php

--- addInuryRecovery.php
+++ addInuryRecovery.php
@@ -7,10 +7,10 @@ if(!$mysqli || $mysqli->connect_errno){
 	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
 	}
 	
-if(!($stmt = $mysqli->prepare("INSERT INTO Injuries(Type, Date, Structure_id, Patient_id) VALUES (?,?,?,?)"))){
+if(!($stmt = $mysqli->prepare("INSERT INTO Injuries(Injury_id, Recovery_id) VALUES (?,?)"))){
 	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
 }
-if(!($stmt->bind_param("ssii",$_POST['Type'],$_POST['date'],$_POST['structureDropDown'],$_POST['patientDropDown']))){
+if(!($stmt->bind_param("ii",$_POST['injuryRecoveryDropDown'],$_POST['patientDropDown']))){
 	echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
 }
 if(!$stmt->execute()){
