28128526272a975265fb5e0b99316c5b74429901
Meadosc <meadosc@onid.orst.edu>
2016-06-05 23:03:42 -0700

    Update FinalProject.php

--- FinalProject.php
+++ FinalProject.php
@@ -248,6 +248,25 @@ if($mysqli->connect_errno){
 			<p>Type of Injury: <input type="text" name="injType"/></p>
 			<p>Date: <input type="text" name="date"/></p>
 			<p>Connected to: <input type="text" name="injType"/></p> <!-- May change this later-->
+			
+			<!-- php to give options for patient_id and enforce foeriegn key constraints -->
+			<?php
+			if(!($stmt = $mysqli->prepare("SELECT id, Fname FROM Patients"))){
+				echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+			}
+			
+			if(!$stmt->execute()){
+				echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+			}
+			if(!$stmt->bind_result($id, $Fname)){
+				echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+			}
+			while($stmt->fetch()){
+				echo '<option value=" '. $id . ' "> ' . $Fname . '</option>\n';
+			}
+			$stmt->close();
+			?>
+			
 			<p><input type="submit" /></p>
 		</fieldset>
 	</form>
