3238e92ead8cef412b2a409195f6d87abc22a1ca
Meadosc <meadosc@onid.orst.edu>
2016-06-06 00:04:51 -0700

    Update FinalProject.php

--- FinalProject.php
+++ FinalProject.php
@@ -316,6 +316,26 @@ if($mysqli->connect_errno){
 				$stmt->close();
 				?>
 			</select><br>
+			Recovery ID:
+			<select name= recoveryDropDown>
+				<!-- php to give options for patient_id and enforce foreign key constraints -->
+				<?php
+				if(!($stmt = $mysqli->prepare("SELECT id, Recovery FROM Recovery"))){
+					echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+				}
+				
+				if(!$stmt->execute()){
+					echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+				}
+				if(!$stmt->bind_result($id, $Recovery)){
+					echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+				}
+				while($stmt->fetch()){
+					echo '<option value=" '. $id . ' "> ' . $Recovery . '</option>\n';
+				}
+				$stmt->close();
+				?>
+			</select>
 			<p><input type="submit" /></p>
 		</fieldset>
 	</form>
