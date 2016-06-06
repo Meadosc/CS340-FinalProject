2d678fec7e25751e3241e632bada6b938f62c411
Meadosc <meadosc@onid.orst.edu>
2016-06-06 00:50:39 -0700

    Create deletePatient.php

---
+++ deletePatient.php
@@ -0,0 +1,21 @@
+<?php
+//Turn on error reporting
+ini_set('display_errors', 'On');
+//Connects to the database
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu","meadosc-db","oXUqqY97BfzGwbV7","meadosc-db");
+if(!$mysqli || $mysqli->connect_errno){
+	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+	
+if(!($stmt = $mysqli->prepare("INSERT INTO Patients(Fname, Lname) VALUES (?,?)"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+if(!($stmt->bind_param("ss",$_POST['Fname'],$_POST['Lname']))){
+	echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
+}
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+} else {
+	echo "Added " . $stmt->affected_rows . " rows to Patients.";
+}
+?>
