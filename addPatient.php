457b5e524123021567e39500a88c6953176603db
Meadosc <meadosc@onid.orst.edu>
2016-06-05 22:16:29 -0700

    Update addPatient.php

--- addPatient.php
+++ addPatient.php
@@ -7,15 +7,15 @@ if(!$mysqli || $msqli->connect_errno){
 	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
 	}
 	
-if(!($stmt = $mysqli->prepare("INSERT INTO bsg_people(fname, lname, homeworld, age) VALUES (?,?,?,?)"))){
+if(!($stmt = $mysqli->prepare("INSERT INTO Patients(Fname, Lname) VALUES (?,?)"))){
 	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
 }
-if(!($stmt->bind_param("ssii",$_POST['FirstName'],$_POST['LastName'],$_POST['Homeworld'],$_POST['Age']))){
+if(!($stmt->bind_param("ss",$_POST['Fname'],$_POST['Lname']))){
 	echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
 }
 if(!$stmt->execute()){
 	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
 } else {
-	echo "Added " . $stmt->affected_rows . " rows to bsg_people.";
+	echo "Added " . $stmt->affected_rows . " rows to Patients.";
 }
 ?>
