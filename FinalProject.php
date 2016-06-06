dc2f927402ea9eaeeda224b76154a27889efe16c
Meadosc <meadosc@onid.orst.edu>
2016-06-06 00:06:34 -0700

    Update FinalProject.php

--- FinalProject.php
+++ FinalProject.php
@@ -297,7 +297,7 @@ if($mysqli->connect_errno){
 		<fieldset>
 			<legend>Injuries_Recovery</legend>
 			Injury ID: 
-			<select name= injuryRecoveryDropDown>
+			<select name= injuryDropDown>
 				<!-- php to give options for injury_id and enforce foreign key constraints -->
 				<?php
 				if(!($stmt = $mysqli->prepare("SELECT id FROM Injuries"))){
